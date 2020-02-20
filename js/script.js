440e486c9f56770b5851d585e2fe3e1d435b56f1
Sarthakchaudhary13 <sarthakchaudhary13@gmail.com>
2020-02-20 22:43:45 +0530

    aa

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 var quotes = ['“The purpose of our lives is to be happy." — <strong>Dalai Lama</strong>','“Life is what happens when you’re busy making other plans.” — <strong>John Lennon</strong>',
                 '“Get busy living or get busy dying.” — <strong>Stephen King</strong>','“You only live once, but if you do it right, once is enough." — <strong>Mae West</strong>',
-                '"If you want to live a happy life, tie it to a goal, not to people or things.”– <strong>Albert Einstein</strong>',
-                '“Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking.” – <strong>Steve Jobs</strong>',
                     '"If you can'+"'t take risks, you can'"+'t create a future."-<strong>Monkey.D.Luffy</strong>' ]
 
 
