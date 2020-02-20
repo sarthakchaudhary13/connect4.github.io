24df8289c57ba3b39377f0ca9714ea4cb85a80a9
Sarthakchaudhary13 <sarthakchaudhary13@gmail.com>
2020-02-20 21:50:03 +0530

    again

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 var quotes = ['“The purpose of our lives is to be happy." — <strong>Dalai Lama</strong>','“Life is what happens when you’re busy making other plans.” — <strong>John Lennon</strong>',
                 '“Get busy living or get busy dying.” — <strong>Stephen King</strong>','“You only live once, but if you do it right, once is enough." — <strong>Mae West</strong>',
                 '"If you want to live a happy life, tie it to a goal, not to people or things.”– <strong>Albert Einstein</strong>',
-                '“Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking.” – <strong>Steve Jobs</strong>']
+                '“Your time is limited, so don’t waste it living someone else’s life. Don’t be trapped by dogma – which is living with the results of other people’s thinking.” – <strong>Steve Jobs</strong>',
+                    '"If you can'+"'t take risks, you can'"+'t create a future."-<strong>Monkey.D.Luffy</strong>' ]
 
 
-var cycle = setInterval(change,6000);
-
-function change() {
-
-
-    $(".quote").animate({'opacity':0}, 4000, function(){
-
-    $(this).html(quotes[Math.floor(Math.random()*quotes.length)]).animate({'opacity': 1}, 2000);
-});
-}
+// var cycle = setInterval(change,4000);
+//
+// function change() {
+//
+//
+//     $(".quote").slideToggle(2000, function(){
+//
+//     $(this).html(quotes[Math.floor(Math.random()*quotes.length)]).slideToggle(2000)
+// });
+// }
