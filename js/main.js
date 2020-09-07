a6330c3c88da86645c5ca38d829c9c94a2fd16e9
Sarthak <sarthak@MacBook.local>
2020-09-08 04:33:59 +0530

    new style

---
+++ js/main.js
@@ -0,0 +1,74 @@
+// function to decide what to do with what commnad
+// and if command is valid or not.
+function resolve_command(command) {
+  if (command == 'clear') {
+    $('.history p').remove()
+  } else if (command == 'help') {
+    printOut(help(), true);
+  } else if (command == 'ls') {
+    printOut(ls(), true);
+  } else if (command.slice(0, 4) == 'cat ') {
+    printOut(cat(command.slice(4)), true)
+  } else {
+    printOut(command, false)
+  }
+}
+
+
+//command help help
+function help() {
+  return "\
+  help&emsp;--&emsp;opens this menu<br>\
+  clear&emsp;--&emsp;clears the console<br>\
+  ls&emsp;--&emsp;list files<br>\
+  cat&emsp;--&emsp;prints content of file in console<br>\
+  "
+}
+
+
+//ls command
+function ls() {
+  return "\
+  about.txt&emsp;&emsp;contact.txt&emsp;&emsp;projects.txt\
+  "
+}
+
+// cat command
+function cat(fname){
+  $.get('../files/'+fname,function(data){
+    if (data.status == 200){
+      printOut(data.responseText,true)
+    }
+    else{
+      printOut('<span class="error">No such file found!</span>',true);
+    }
+  });
+}
+// prints output returned by commnad functions
+function printOut(out, success) {
+  if (success) $('.history').append('<p class="success">' + out + ' </p>');
+  else $('.history').append('<p class="error">Error: ' + out + ', command not found!<br>see help</p>');
+}
+
+function updateScroll() {
+  var element = document.getElementById("terminal");
+  element.scrollTop = element.scrollHeight;
+  element.scrollTop = element.scrollHeight;
+}
+
+$(function () {
+  $('#cursor').blink(1)
+  $(document).on("keypress", function (e) {
+    if (e.key != "Enter") {
+      $('#text').append(e.key);
+    } else {
+      var tmp = $('.command').clone();
+      tmp.find('#cursor').remove();
+      $('.history').append(tmp.prop('outerHTML'));
+      updateScroll()
+      resolve_command($('#text').text());
+      $('.history p #text').removeAttr('id');
+      $('#text').text("")
+    }
+  })
+});
